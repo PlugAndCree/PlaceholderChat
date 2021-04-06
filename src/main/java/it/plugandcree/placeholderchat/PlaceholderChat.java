89d12c075e08e0b412c7bdb69d4ce9366ffd6b0c
PlugAndCree <nicolamarangon123@gmail.com>
2021-04-06 15:26:43 +0200

    Added custom configs

--- src/main/java/it/plugandcree/placeholderchat/PlaceholderChat.java
+++ src/main/java/it/plugandcree/placeholderchat/PlaceholderChat.java
@@ -1,7 +1,66 @@
 package it.plugandcree.placeholderchat;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import it.plugandcree.placeholderchat.config.CustomConfig;
+
+
 public class PlaceholderChat extends JavaPlugin {
+	
+	private static PlaceholderChat instance;
+	private CustomConfig langConfig;
+	private CustomConfig mainConfig;
+	
+	@Override
+	public void onEnable() {
+		instance = this;
+		
+		reloadConfig();
+	}
+	
+	public void reloadConfig() {
+		setLangConfig(createConfigFile("lang.yml"));
+		setMainConfig(createConfigFile("config.yml"));
+	}
+	
+	private CustomConfig createConfigFile(String name) {
+		File fc = new File(getDataFolder(), name);
+		if (!fc.exists()) {
+			fc.getParentFile().mkdirs();
+			saveResource(name, false);
+		}
+
+		CustomConfig configFile = new CustomConfig();
+		try {
+			configFile.load(fc);
+			return configFile;
+		} catch (IOException | InvalidConfigurationException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static PlaceholderChat getInstance() {
+		return instance;
+	}
+
+	public CustomConfig getLangConfig() {
+		return langConfig;
+	}
+
+	public void setLangConfig(CustomConfig langConfig) {
+		this.langConfig = langConfig;
+	}
+
+	public CustomConfig getMainConfig() {
+		return mainConfig;
+	}
+
+	public void setMainConfig(CustomConfig mainConfig) {
+		this.mainConfig = mainConfig;
+	}
 	
 }
