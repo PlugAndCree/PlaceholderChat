14b7f0de3c793151d297ac03be507b083fd37937
PlugAndCree <nicolamarangon123@gmail.com>
2022-10-20 19:33:21 +0200

    Switched to minimessage

--- src/main/java/it/plugandcree/placeholderchat/events/PlayerChat.java
+++ src/main/java/it/plugandcree/placeholderchat/events/PlayerChat.java
@@ -11,17 +11,19 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import dev.vankka.enhancedlegacytext.EnhancedLegacyText;
 import it.plugandcree.placeholderchat.PlaceholderChat;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class PlayerChat implements Listener {
 
+	private static final Pattern PATTERN_HEX = Pattern.compile("[§&](#[0-9a-fA-F]{6})");
+	private static final Pattern PATTERN_GRADIENT = Pattern.compile("\\{(([&§][0-9a-f]|[&§]#[0-9a-f]{6})(\\,[&§][0-9a-f]|\\,[&§]#[0-9a-f]{6})+)\\}");
+	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerChat(AsyncPlayerChatEvent e) {
-
+		 
 		if (e.isCancelled())
 			return;
 
@@ -46,7 +48,9 @@ public class PlayerChat implements Listener {
 
 		format = format.replace("%prefix%", prefix).replace("%suffix%", suffix);
 
+		format = format.replace('&',(char) 1);
 		format = PlaceholderAPI.setPlaceholders(e.getPlayer(), format);
+		format = format.replace((char) 1, '&');
 
 		try {
 			e.setFormat(format);
@@ -55,60 +59,27 @@ public class PlayerChat implements Listener {
 					.severe(String.format("Some placeholder in group %s does not exist", perms));
 			return;
 		}
-
+		
 		e.setCancelled(true);
+		
+		MiniMessage mm = MiniMessage.miniMessage();
 
-		String[] adventureString = e.getFormat().split("%s");
-		Component adventureComponent = EnhancedLegacyText.get().buildComponent("").build();
-
-		// %s > %s - len = 2 splitted
-		// ''
-		// ' > '
+		Component adventureComponent = mm.deserialize(legacyToMinimessage(
+				e.getFormat().replaceFirst("%s", "<hover:show_text:'" + placeholders(e.getPlayer(), PlaceholderChat.getInstance().getMainConfig().getRawString("user-hover-text")) + "'>" + e.getPlayer().getDisplayName() + "</hover>")));
 		
-		// something %s > %s - len = 2 splitted
-		// 'something '
-		// ' > '
+		Component chat;
 		
-		// %s > %s something - len = 3 splitted
-		// ''
-		// ' > '
-		// ' something'
+		if (e.getPlayer().hasPermission("placeholderchat.colorchat")) 
+			chat = mm.deserialize(legacyToMinimessage(e.getMessage()));
+		else 
+			chat = Component.text(e.getMessage());
 		
-		// something %s > %s something - len = 3 splitted
-		// 'something '
-		// ' >asdasdasdsda '
-		// ' something'
-
+		adventureComponent = adventureComponent.replaceText(b -> {
+			b.matchLiteral("%s");
+			b.once();
+			b.replacement(c -> c.content("").append(chat));
+		});
 		
-		adventureComponent = adventureComponent
-				.append(EnhancedLegacyText.get().buildComponent(adventureString[0]).build());
-		
-		//Player name component and hover event
-		adventureComponent = adventureComponent
-				.append(EnhancedLegacyText.get().buildComponent(getLastColor(adventureString[0]) + e.getPlayer().getDisplayName()).build()
-						.hoverEvent(HoverEvent.showText(
-								EnhancedLegacyText.get().buildComponent(PlaceholderAPI.setPlaceholders(e.getPlayer(),
-										PlaceholderChat.getInstance().getMainConfig().getRawString("user-hover-text"))
-
-								).build().asComponent())));
-		
-		//Chat separator between player name and message
-		adventureComponent = adventureComponent
-				.append(EnhancedLegacyText.get().buildComponent(adventureString[1]).build());
-				
-		//Chat message
-		if (e.getPlayer().hasPermission("placeholderchat.colorchat"))
-			adventureComponent = adventureComponent
-				.append(EnhancedLegacyText.get().buildComponent(getLastColor(adventureString[1]) + e.getMessage()).build());
-		else
-			adventureComponent = adventureComponent
-				.append(EnhancedLegacyText.get().buildComponent(getLastColor(adventureString[1]) + stripColor(e.getMessage())).build());
-		
-		
-		if (adventureString.length == 3)
-			adventureComponent = adventureComponent.append(
-					EnhancedLegacyText.get().buildComponent(getLastColor(adventureString[1]) + adventureString[2]).build());
-
 		for (Player p : e.getRecipients()) {
 			PlaceholderChat.getInstance().getAdventure().player(p).sendMessage(adventureComponent);
 		}
@@ -116,36 +87,65 @@ public class PlayerChat implements Listener {
 		Bukkit.getLogger().info(e.getPlayer().getName() + " > " + e.getMessage());
 	}
 	
-	private String getLastColor(String message) {
-		  String regex = ".*([&§][0-9a-fA-F]|[&§]#[0-9a-fA-F]{6})|.*([&§][olknm])";
-		  Pattern p = Pattern.compile(regex);
-		  Matcher m = p.matcher(message);
-		  
-		  if(!m.find())
-			  return "";
-		  
-		  String g1 = m.group(1);
-		  String g2 = m.group(2);
-		  
-		  if(String.valueOf(g1).equals("null"))
-			  g2 = "";
-		  
-		  if(String.valueOf(g2).equals("null"))
-			  g2 = "";
-		  
-		  return g1 + g2;	
+	private String placeholders(Player p, String message) {
+		return PlaceholderAPI.setPlaceholders(p, legacyToMinimessage(message));
 	}
 	
-	private String stripColor(String message) {
-		String regex = "([&§][0-9a-fA-F]|[&§]#[0-9a-fA-F]{6})|([&§][olknm])";
+	private String legacyToMinimessage(String message) {
+		Matcher matcherGradient = PATTERN_GRADIENT.matcher(message);
+		while (matcherGradient.find()) {
+			String found = matcherGradient.group(1);
+			found = found.replaceAll("[&§]0", "black")
+					.replaceAll("[&§]1", "dark_blue")
+					.replaceAll("[&§]2", "dark_green")
+					.replaceAll("[&§]3", "dark_aqua")
+					.replaceAll("[&§]4", "dark_red")
+					.replaceAll("[&§]5", "dark_purple")
+					.replaceAll("[&§]6", "gold")
+					.replaceAll("[&§]7", "gray")
+					.replaceAll("[&§]8", "dark_gray")
+					.replaceAll("[&§]9", "blue")
+					.replaceAll("[&§]a", "green")
+					.replaceAll("[&§]b", "aqua")
+					.replaceAll("[&§]c", "red")
+					.replaceAll("[&§]d", "light_purple")
+					.replaceAll("[&§]e", "yellow")
+					.replaceAll("[&§]f", "white")
+					.replaceAll("[&§]#", "#")
+					.replace(',', ':');
+			
+			
+			message = message.replaceFirst(PATTERN_GRADIENT.toString(), "<gradient:" + found + ">");
+		}
 		
-		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(message);
-		  
-		if(!m.find())
-			return message;
-		 
-		return m.replaceAll("");
+		Matcher matcherHex = PATTERN_HEX.matcher(message);
+		
+		while (matcherHex.find())
+			message = message.replaceFirst(PATTERN_HEX.toString(), "<color:" + matcherHex.group(1) + ">");
+		
+		message = message.replaceAll("[&§]0", "<black>")
+				.replaceAll("[&§]1", "<dark_blue>")
+				.replaceAll("[&§]2", "<dark_green>")
+				.replaceAll("[&§]3", "<dark_aqua>")
+				.replaceAll("[&§]4", "<dark_red>")
+				.replaceAll("[&§]5", "<dark_purple>")
+				.replaceAll("[&§]6", "<gold>")
+				.replaceAll("[&§]7", "<gray>")
+				.replaceAll("[&§]8", "<dark_gray>")
+				.replaceAll("[&§]9", "<blue>")
+				.replaceAll("[&§]a", "<green>")
+				.replaceAll("[&§]b", "<aqua>")
+				.replaceAll("[&§]c", "<red>")
+				.replaceAll("[&§]d", "<light_purple>")
+				.replaceAll("[&§]e", "<yellow>")
+				.replaceAll("[&§]f", "<white>")
+				.replaceAll("[&§]l", "<bold>")
+				.replaceAll("[&§]m", "<strikethrough>")
+				.replaceAll("[&§]n", "<underlined>")
+				.replaceAll("[&§]o", "<italic>")
+				.replaceAll("[&§]k", "<obfuscated>")
+				.replaceAll("[&§]r", "<reset>");
+		
+		return message;
 	}
-	
 }
